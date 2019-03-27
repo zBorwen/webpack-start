37d189a0a49abd08abad121e9808d667899a055a
zBorwen <136676959@qq.com>
2019-03-27 17:09:33 +0800

    修改stylelint配置
    添加stylelint-webpack-plugin插件

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-  "extends": ["stylelint-config-standard", "stylelint-config-recess-order"],
-  "piugins": ["stylelint-order", "stylelint-scss"],
-  "rules": {
-    "at-rule-no-unknown": [true, {
-      "ignoreAtRules": [
-        "mixin", "extend", "content"
+  'extends': ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  'piugins': ['stylelint-order', 'stylelint-scss'],
+  'rules': {
+    'at-rule-no-unknown': [true, {
+      'ignoreAtRules': [
+        'mixin', 'extend', 'content'
       ]
     }],
-    "order/order": [
-      "custom-properties",
-      "declarations"
+    'order/order': [
+      'custom-properties',
+      'declarations'
     ],
-    "order/properties-alphabetical-order": true,
-    "at-rule-empty-line-before": null
+    'order/properties-alphabetical-order': true,
+    'at-rule-empty-line-before': null
   }
 }
